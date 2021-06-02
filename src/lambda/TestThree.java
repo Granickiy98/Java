360e39a1cb7b5d262551b0a61661e12823749b81
Granickiy <puto45mexu>
2021-06-02 15:42:21 +0300

    Supplire

---
+++ src/lambda/TestThree.java
@@ -0,0 +1,39 @@
+package lambda;
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
+public class TestThree {
+    public static ArrayList<Car> createThreeCars(Supplier<Car> carSupplier){
+        ArrayList<Car> al = new ArrayList<>();
+        for (int i=0;i<3;i++){
+            al.add(carSupplier.get());
+        }
+        return al;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Car> carArrayList = createThreeCars(()->new Car("Opel Omega","Black",2.0));
+        System.out.println("Our cars: "+carArrayList);
+    }
+}
+
+class Car{
+    String model;
+    String color;
+    double engine;
+
+    public Car(String model, String color, double engine) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", engine=" + engine +
+                '}';
+    }
+}
