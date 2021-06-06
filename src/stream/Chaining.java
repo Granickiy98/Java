3cdaf2d8916e8b5d502168ce8008caaf17d73e4b
Granickiy <puto45mexu>
2021-06-06 23:30:18 +0300

    Stream:concat

--- src/stream/Chaining.java
+++ src/stream/Chaining.java
@@ -11,5 +11,10 @@ public class Chaining {
 //            System.out.println("!!!");
 //            return el%2==0l;
 //        }).collect(Collectors.toList());
+
+        Stream<Integer> stream2 = Stream.of(1,2,3,4,5);
+        Stream<Integer> stream3 = Stream.of(6,7,8,9,10);
+        Stream<Integer> stream4 = Stream.concat(stream2,stream3);
+        stream4.forEach(System.out::println);
     }
 }
