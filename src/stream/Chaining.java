5134bcd5797093a95b952c2861a1657b3f3fa973
Granickiy <puto45mexu>
2021-06-06 23:33:11 +0300

    Stream:distinct

--- src/stream/Chaining.java
+++ src/stream/Chaining.java
@@ -12,9 +12,12 @@ public class Chaining {
 //            return el%2==0l;
 //        }).collect(Collectors.toList());
 
-        Stream<Integer> stream2 = Stream.of(1,2,3,4,5);
-        Stream<Integer> stream3 = Stream.of(6,7,8,9,10);
-        Stream<Integer> stream4 = Stream.concat(stream2,stream3);
-        stream4.forEach(System.out::println);
+//        Stream<Integer> stream2 = Stream.of(1,2,3,4,5);
+//        Stream<Integer> stream3 = Stream.of(6,7,8,9,10);
+//        Stream<Integer> stream4 = Stream.concat(stream2,stream3);
+//        stream4.forEach(System.out::println);
+
+        Stream<Integer> stream5 = Stream.of(1,2,3,4,5,1,2,3);
+        stream5.distinct().forEach(System.out::println);
     }
 }
