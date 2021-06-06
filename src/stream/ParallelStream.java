5bbb015825f5d8a6ec6ad6b318662d56956a85c8
Granickiy <puto45mexu>
2021-06-07 00:33:56 +0300

    Stream:parallelStream

---
+++ src/stream/ParallelStream.java
@@ -0,0 +1,21 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParallelStream {
+    public static void main(String[] args) {
+        List<Double> list = new ArrayList<>();
+        list.add(10.0);
+        list.add(5.0);
+        list.add(1.0);
+        list.add(0.25);
+
+        double sumResult = list.parallelStream()
+                .reduce(((a, e) -> a+e)).get();
+        System.out.println("sumResult= " +sumResult);
+        double divisionResult = list.stream()
+                .reduce(((a, e) -> a/e)).get();
+        System.out.println("divisionResult= " +divisionResult);
+    }
+}
