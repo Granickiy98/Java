ea0d4d9aa079a36bf91d3a60b34fa1c0a9a3570c
Granickiy <puto45mexu>
2021-06-09 14:24:39 +0300

    MultiThreading:Data race: synchronized

---
+++ src/multithreading/Ex11.java
@@ -0,0 +1,26 @@
+package multithreading;
+
+public class Ex11 {
+    volatile static  int counter =0;
+    public static synchronized void increment(){counter++;}
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new MyRunnable());
+        Thread thread2 = new Thread(new MyRunnable());
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(counter);
+    }
+}
+
+class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0 ; i<1000; i++){
+            Ex11.increment();
+        }
+    }
+}
