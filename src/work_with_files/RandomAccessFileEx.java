dcdee2209ad8c75a17157ee978cca6d1c6208963
Granickiy <puto45mexu>
2021-06-17 14:23:18 +0300

    RandomAccessFile

---
+++ src/work_with_files/RandomAccessFileEx.java
@@ -0,0 +1,30 @@
+package work_with_files;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class RandomAccessFileEx {
+    public static void main(String[] args) {
+        try(RandomAccessFile file = new RandomAccessFile("test10.txt","rw")) {
+            String s1 = file.readLine();
+            System.out.println(s1);
+
+            file.seek(200);
+            String s2 = file.readLine();
+            System.out.println(s2);
+
+        long position = file.getFilePointer();
+            System.out.println(position);
+
+            file.seek(file.length()-1);
+            file.writeBytes("\n\t\t\t\t\tWilliam Butler Yeats");
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
